a73fcaed9d69c2b1c12826e54309087f0c679fe2
zhou u74a8 <1571224208@qq.com>
2021-09-21 22:31:09 +0800

    trying browser support

---
+++ src/org/e792a8/acme/ui/browser/BrowserSupport.java
@@ -0,0 +1,22 @@
+package org.e792a8.acme.ui.browser;
+
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.browser.AbstractWorkbenchBrowserSupport;
+import org.eclipse.ui.browser.IWebBrowser;
+
+public class BrowserSupport extends AbstractWorkbenchBrowserSupport {
+
+	@Override
+	public IWebBrowser createBrowser(int style, String browserId, String name, String tooltip)
+		throws PartInitException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IWebBrowser createBrowser(String browserId) throws PartInitException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
